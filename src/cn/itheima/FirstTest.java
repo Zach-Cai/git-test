841433c4dd53ef4c1b0c1e9833d9f093d4dd170b
zhangsan <zhangsan@itcast.cn>
2021-06-08 17:47:50 +0800

    IDEA中将Test项目提交到本地仓库中

---
+++ src/cn/itheima/FirstTest.java
@@ -0,0 +1,10 @@
+package cn.itheima;
+
+public class FirstTest {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
