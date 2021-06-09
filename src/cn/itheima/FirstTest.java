69cbc9ea1bcbd665446ded49f3ab2d7a00482a30
zhangsan <zhangsan@itcast.cn>
2021-06-09 10:19:20 +0800

    在idea修改clone下来的代码

--- src/cn/itheima/FirstTest.java
+++ src/cn/itheima/FirstTest.java
@@ -6,10 +6,12 @@ public class FirstTest {
         System.out.println("hello git project");
         System.out.println("hello git project");
         System.out.println("hello git project");
+//        hello
     }
     public static void test(){
         int i=0;
         i +=10;
         System.out.println(i);
+        System.out.println(i+10);
     }
 }
